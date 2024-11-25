b7416100fb53b470a7f56026c6c58d1a51735971
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-25 10:03:55 +0100

    demo 4

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,8 @@
 // File: 0-reset_to_98.c
 
 #include <stdio.h>
-
+#include "main.h"
 void reset_to_98(int *n)
 {
     *n = 98;
-}
-
-int main(void)
-{
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
 }
