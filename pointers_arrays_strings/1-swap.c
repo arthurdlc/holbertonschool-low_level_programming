7df4345e3d01b997cb6d749481debf8addc56c2f
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-26 11:22:20 +0100

    ccdcd

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Updates the value pointed to by the pointer to 98.
- * @ptr1: Pointer to an integer to swap with ptr2.
- * @ptr2: Pointer to an integer to swap with ptr1.
+ * swap_int - Swaps the values of two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
  */
+void swap_int(int *a, int *b)
+{
+	int temp;
 
-void swap_numbers(int *ptr1, int *ptr2) {
-  int temp;
-  temp = *ptr1;
-  *ptr1 = *ptr2;
-  *ptr2 = temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
