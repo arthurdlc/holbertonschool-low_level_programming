1bdbc9d2955f257f982b2df0384ca8c9db03116c
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-26 14:48:26 +0100

    cklshVCE

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,17 +6,17 @@
  */
 void rev_string(char *s)
 {
-    int len = 0;
-    int i;
-    char tmp;
+int len = 0;
+int i;
+char tmp;
 
-    while (s[len] != '\0')
-        len++;
+while (s[len] != '\0')
+len++;
 
-    for (i = 0; i < len / 2; i++)
-    {
-        tmp = s[i];
-        s[i] = s[len - i - 1];
-        s[len - i - 1] = tmp;
-    }
+for (i = 0; i < len / 2; i++)
+{
+tmp = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = tmp;
+}
 }
