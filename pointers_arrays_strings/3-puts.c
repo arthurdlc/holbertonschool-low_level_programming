09db842827c84f81a184aa94e95298cdd7026e97
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-26 14:36:40 +0100

    cdqkhf

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -8,12 +8,12 @@
  */
 void _puts(char *str)
 {
-	int count = 0;
+int count = 0;
 
-	while (str[count] != '\0')
-	{
-		_putchar(str[count]);
-		count++;
-	}
-	_putchar('\n');
+while (str[count] != '\0')
+{
+_putchar(str[count]);
+count++;
+}
+_putchar('\n');
 }
