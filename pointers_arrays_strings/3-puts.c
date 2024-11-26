4e1a50bc6dc4debccac183081167b05c75d88399
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-26 12:37:05 +0100

    dcdcd

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -10,9 +10,9 @@ void _puts(char *str)
     /* Parcours de la chaîne jusqu'à atteindre le caractère null ('\0') */
     while (*str != '\0')
     {
-        _putchar(*str);  /* Affiche chaque caractère de la chaîne */
-        str++;           /* Passe au caractère suivant */
+        _putchar(*str); 
+        str++;           
     }
 
-    _putchar('\n'); /* Affiche une nouvelle ligne après la chaîne */
+    _putchar('\n'); 
 }
