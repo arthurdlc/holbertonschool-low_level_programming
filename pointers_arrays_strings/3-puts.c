ca3035d20bc18a1dc2f0a0737e36c45312bb246e
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-26 12:33:00 +0100

    cdhcd

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,18 +1,12 @@
 #include "main.h"
-/**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: the string to print
- * Return: void
-**/
 
 void _puts(char *str)
 {
-
     while (*str != '\0')
     {
-        _putchar(*str);
-        str++;
+        _putchar(*str);  /* Affiche chaque caractère de la chaîne */
+        str++;           /* Avance au caractère suivant */
     }
 
-    _putchar('\n');
+    _putchar('\n'); /* Affiche un saut de ligne à la fin */
 }
