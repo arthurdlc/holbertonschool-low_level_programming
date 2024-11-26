818e953435259aeccd7e459d62b9f1001e4f7fba
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-26 10:52:48 +0100

    dxeoed

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,10 @@
+/* File: 0-reset_to_98.c */
+
 #include "main.h"
 
 /**
- * reset_to_98 - Updates the value an int pointer points to, to 98.
- * @n: Pointer to an int.
+ * reset_to_98 - Met à jour la valeur pointée par le pointeur à 98.
+ * @n: Pointeur vers un entier à modifier.
  */
 void reset_to_98(int *n)
 {
