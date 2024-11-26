702c375bd219d3ac6f252e74c46e6a9b5cb7126d
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-26 16:06:59 +0100

    cddss

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -12,6 +12,7 @@ int i;
 char tmp;
 
 while (s[len] != '\0')
+{
 len++;
 
 for (i = 0; i < len / 2; i++)
@@ -19,5 +20,6 @@ for (i = 0; i < len / 2; i++)
 tmp = s[i];
 s[i] = s[len - i - 1];
 s[len - i - 1] = tmp;
+}
 }
 }
