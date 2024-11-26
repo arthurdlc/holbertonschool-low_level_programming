53b83f02834a3e0674f16ee5810ee52881ab9905
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-26 14:45:12 +0100

    dsogdq

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Prints a string in reverse, followed by a new line.
+ * @s: The string to be printed in reverse.
+ */
+void print_rev(char *s)
+{
+int length = 0;
+
+while (s[length] != '\0')
+length++;
+
+for (length--; length >= 0; length--)
+_putchar(s[length]);
+
+_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ void reset_to_98(int *n);
 int _putchar(char c);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 
 #endif /* MAIN_H */
