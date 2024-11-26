04d5f93cd74b4a1745fd023d1a3669f9e98ef7b3
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-26 12:11:13 +0100

    dedjd

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,21 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * _strlen - renvoiie le taille d'une chaine de caractere.
- * @s: la chaine de caractere.
+ * _strlen - Returns the length of a string.
+ * @s: The string to measure (pointer to the first character).
+ *
+ * Return: The length of the string (number of characters before '\0').
  */
 int _strlen(char *s)
 {
-    return (strlen(s)); 
+	int length = 0; /* Initialize the length counter to 0 */
+
+	/* Loop through the string until the null terminator is reached */
+	while (s[length] != '\0')
+	{
+		length++; /* Increment the length counter for each character */
+	}
+
+	/* Return the total length of the string */
+	return (length);
 }
