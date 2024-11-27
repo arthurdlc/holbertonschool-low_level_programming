89687eaf56059f0a77be1fdad104d25f4639330a
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-27 11:03:28 +0100

    ddedcsc

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h> // Inclusion correcte de l'en-tête
+#include <stdio.h> /* Inclusion correcte de l'en-tête */
 
 /**
  * print_array - Prints n elements of an array of integers
@@ -12,9 +12,9 @@ int i;
 
 for (i = 0; i < n; i++)
 {
-printf("%d", a[i]); // Imprime un entier
-if (i < n - 1) // Ajouter une virgule et un espace sauf pour le dernier élément
+printf("%d", a[i]); /* Imprime un entier */
+if (i < n - 1) /* Ajouter une virgule et un espace sauf pour le dernier élément */
 printf(", ");
 }
-printf("\n"); // Imprime une nouvelle ligne (corrigé)
+printf("\n"); /* Imprime une nouvelle ligne */
 }
