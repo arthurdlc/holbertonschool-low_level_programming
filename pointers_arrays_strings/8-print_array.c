7322f6c658564d14f89ca1a4051c292f0c679a6c
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-27 10:57:38 +0100

    dqQZ

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,11 @@ void print_array(int *a, int n)
 int i;
 for(i = 0; i < n; i++)
 {
-_printf("%d", a[i]);
+printf("%d", a[i]);
 if(i < n - 1 )
 {
-_printf(", ");
+printf(", ");
 }
 }
-_printf('\n');
+printf('\n');
 }
