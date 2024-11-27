69bc0dcc5c4b6b1c2dde418fa27c0cf3a8a7c496
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-27 10:52:28 +0100

    cds

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string to be measured
+ * Return: the length of the string
+ */
+void print_array(int *a, int n)
+{
+int i;
+for(i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if(i < n - 1 )
+{
+printf(", ");
+}
+}
+printf('\n');
+}
