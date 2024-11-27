9c5d9deff6c1575a29aceb013c38684dfeff387b
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-27 10:07:57 +0100

    rdce

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/*
+*
+*
+*
+*/
+void puts_half(char *str)
+{
+    int len = 0, i, n;
+    while(str[len] != 0 )
+    {
+        len ++;
+    }
+    if(len % 2 == '\0')
+    {
+        n = len/2;
+    }
+    else
+    {
+        n = (len - 1) / 2 + 1;
+    }
+    for(i = n; i < len; i++)
+    {
+        putchar(str[i]);
+    }
+    putchar('\n');
+}
