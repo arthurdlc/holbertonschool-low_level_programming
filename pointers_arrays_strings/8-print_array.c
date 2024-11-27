a29e0a6d042d33f2ef32ae1383c4f539fbf40332
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-27 11:01:48 +0100

    cscds

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,20 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdio.h> // Inclusion correcte de l'en-tête
+
 /**
- * puts2 - prints every other character of a string
- * @str: the string to be measured
- * Return: the length of the string
+ * print_array - Prints n elements of an array of integers
+ * @a: Pointer to the array
+ * @n: Number of elements to print
  */
 void print_array(int *a, int n)
 {
 int i;
-for(i = 0; i < n; i++)
+
+for (i = 0; i < n; i++)
 {
-printf("%d", a[i]);
-if(i < n - 1 )
-{
+printf("%d", a[i]); // Imprime un entier
+if (i < n - 1) // Ajouter une virgule et un espace sauf pour le dernier élément
 printf(", ");
 }
-}
-printf('\n');
+printf("\n"); // Imprime une nouvelle ligne (corrigé)
 }
